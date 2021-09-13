b25b56c26d5952858057e5eeb218808b5d9f16e5
ghksxk1002 <ghksxk1002@naver.com>
2021-09-13 10:40:06 +0900

    해냈다

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -1,66 +1,26 @@
 package prob05;
 
 public class Account {
+
 	private String accountNo;
 	private int balance = 0;
-<<<<<<< HEAD
-
-	public Account(String accontNo) {
-		this.accountNo = accontNo;
-	}
-
-	public void save(int balance) {
-
-		this.balance += balance;
-
-	}
-
-	public void deposit(int balance) {
-
-		this.balance -= balance;
-	}
-
-	public String getAccountNo() {
-		return accountNo;
-	}
-
-	public void setAccountNo(String accountNp) {
-		this.accountNo = accountNp;
-	}
-
-	public int getBalance() {
-		return balance;
-	}
-
-	public void setBalance(int balance) {
-		this.balance = balance;
-	}
-
-=======
 	
 	
-	public Account(String accontNo) {
-		this.accountNo = accontNo;	
-	}
 	
-	public void save(int balance) {
+	
+	
+	public Account(String accountNo) {
 		
-		this.balance += balance;
+		this.accountNo = accountNo;
 		
 	}
-
-	public void deposit(int balance) {
-		
-		this.balance -= balance;
-	}
-	
 	
 	
 	public String getAccountNo() {
 		return accountNo;
 	}
-	public void setAccountNo(String accountNp) {
-		this.accountNo = accountNp;
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
 	}
 	public int getBalance() {
 		return balance;
@@ -68,20 +28,14 @@ public class Account {
 	public void setBalance(int balance) {
 		this.balance = balance;
 	}
-
-
-
-
-
-	
-
-
-
-
 	
 	
-	
-	
-	
->>>>>>> branch 'master' of https://github.com/ghksxk1002/javastudy.git
+	public void save(int save) {	
+		this.balance += save;
+	}
+	public void deposit(int deposit) {
+		this.balance -= deposit;
+	}
+
+
 }
