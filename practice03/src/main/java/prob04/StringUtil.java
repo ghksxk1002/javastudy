e301bfa24383cb3bbcfc0a116c2b0fe729f8df7e
ghksxk1002 <ghksxk1002@naver.com>
2021-09-13 14:38:47 +0900

    해냈다

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -2,30 +2,16 @@ package prob04;
 
 public class StringUtil {
 
-<<<<<<< HEAD
-
-	public static String[] concatenate(String[] strArr) {
-		
-		
-		String[] string = new String[strArr.length];
-		for(int i = 0; i < strArr.length ; i++) {
-			   string[i] = strArr[strArr.length-i];
-		}
-		return string;
-=======
 	public static String concatenate(String[] strArr) {
-			
-		
-			String arr = "";
-			for(int i = 0; i < strArr.length; i++) {
-				
-				arr += strArr[i];
-				
-			}
-		
-		
-		
+
+		String arr = "";
+		for (int i = 0; i < strArr.length; i++) {
+
+			arr += strArr[i];
+
+		}
+
 		return arr;
->>>>>>> branch 'master' of https://github.com/ghksxk1002/javastudy.git
+
 	}
 }
