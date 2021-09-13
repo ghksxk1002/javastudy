7319e95b4dcbf4cc987fe2de1e283bda6cfe4357
ghksxk1002 <ghksxk1002@naver.com>
2021-09-13 09:09:49 +0900

    test

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,23 @@
 package prob5;
 
 public class MyStack {
+
+	public MyStack(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void push(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
