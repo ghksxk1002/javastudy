7bc922934a9a4fdcaa1691de408766326a5d0af3
ghksxk1002 <ghksxk1002@naver.com>
2021-09-15 17:17:47 +0900

    NSLookup 해결

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,11 +2,15 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String line = "www.naver.com";
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("도메인을 입력하세요: ");
+		String line = sc.nextLine();
 		
 		try {
 			InetAddress[] inetaddresses = InetAddress.getAllByName(line);
