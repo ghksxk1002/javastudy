d718fd3d7cb9522a760344dc171ab403a5472891
ghksxk1002 <ghksxk1002@naver.com>
2021-09-16 01:40:31 +0900

    다시해야됨

--- finalTest/src/main/java/prob01/Gugudan.java
+++ finalTest/src/main/java/prob01/Gugudan.java
@@ -1,7 +1,9 @@
 package prob01;
 
+import java.util.HashSet;
 import java.util.Scanner;
-//hashset 사용 심화였음ㅣㅣㅣ
+
+//hashset 사용 심화
 public class Gugudan {
 
 	static int resultNumber = 0;
@@ -12,9 +14,9 @@ public class Gugudan {
 
 		resultNumber = l * r;
 
-		int[] answerNumbers = randomizeAnswers();//answerNumber 변수에 렌덤한값 저장
+		int[] answerNumbers = randomizeAnswers();// answerNumber 변수에 렌덤한값 저장
 		int loc = randomize(0, 8);
-		answerNumbers[loc] = resultNumber;//인자값 0`8까지 렌덤하게 l*r 값저장
+		answerNumbers[loc] = resultNumber;// 인자값 0`8까지 렌덤하게 l*r 값저장
 
 		System.out.println(l + " x " + r + " = ?");
 
@@ -46,14 +48,21 @@ public class Gugudan {
 
 	private static int[] randomizeAnswers() {
 		/* 코드 작성(수정 가능) */
-		
-		
+		HashSet<Integer> rNums = new HashSet<>();
 		final int COUNT_ANSWER_NUMBER = 9;
 		int[] boardNumbers = new int[COUNT_ANSWER_NUMBER];
-		for(int i = 0; i < COUNT_ANSWER_NUMBER; i++ ) {
-			boardNumbers[i] = randomize(1, 9);
+
+		while (rNums.size() < COUNT_ANSWER_NUMBER) {
+			int l = randomize(1, 9);
+			int r = randomize(1, 9);
+			rNums.add(l * r);
 		}
-		
+		for(int rNum : rNums) {
+			int i = 0;
+			boardNumbers[++i] = rNum;
+			System.out.println(rNum + " "+boardNumbers[i]);
+		}
+	//모르겠다
 		return boardNumbers;
 	}
 }
