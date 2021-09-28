5bf285c82a7466a66a0782c38f0e045bda3de16d
ghksxk1002 <ghksxk1002@naver.com>
2021-09-28 17:14:03 +0900

    에코

--- network/src/main/java/echo/EchoClient.java
+++ network/src/main/java/echo/EchoClient.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 public class EchoClient {
 	private static final String SERVER_IP = "127.0.0.1";
-	private static final int SERVER_PORT = 6000;
+	private static final int SERVER_PORT = 7000;
 
 	public static void main(String[] args) {
 
--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -7,7 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	private static final int SERVER_PORT = 6000;
+	private static final int SERVER_PORT = 7000;
 
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
